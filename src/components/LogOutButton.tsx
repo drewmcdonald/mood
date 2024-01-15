e0d1f58b20eb7c3b922018317e2175ca1ee67a94
Drew McDonald <drew@watershedclimate.com>
2024-01-15 12:23:39 -0500

    lighthouse fixes.

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -13,6 +13,7 @@ export function LogOutButton() {
     <Tooltip>
       <TooltipTrigger className="fixed bottom-8 right-8 cursor-pointer">
         <Button
+          aria-label="Log out"
           className="shadow-md6 h-16 w-16 rounded-full bg-slate-200 p-3 opacity-85 transition-all duration-300 ease-in-out hover:-translate-x-1 hover:-translate-y-1 hover:scale-110 hover:bg-current hover:bg-opacity-95 hover:shadow-xl dark:bg-slate-700"
           onClick={() => void client.auth.signOut()}
         >
@@ -21,7 +22,7 @@ export function LogOutButton() {
       </TooltipTrigger>
       <TooltipContent
         side="left"
-        className="text-md mb-2 mr-3 animate-fade-in text-primary opacity-85 shadow-md"
+        className="text-md animate-fade-in-95 mb-2 mr-3 text-primary opacity-95 shadow-md"
       >
         Log out
       </TooltipContent>
--- tailwind.config.js
+++ tailwind.config.js
@@ -65,15 +65,15 @@ export const theme = {
         from: { height: "var(--radix-accordion-content-height)" },
         to: { height: "0" },
       },
-      "fade-in": {
+      "fade-in-95": {
         from: { opacity: "0" },
-        to: { opacity: "1" },
+        to: { opacity: "0.95" },
       },
     },
     animation: {
       "accordion-down": "accordion-down 0.2s ease-out",
       "accordion-up": "accordion-up 0.2s ease-out",
-      "fade-in": "fade-in 0.4s ease-in",
+      "fade-in-95": "fade-in-95 0.4s ease-in",
     },
   },
 };

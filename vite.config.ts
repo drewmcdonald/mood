1d04e958af12c7e1d6551b52f011c3153e1325f5
Drew McDonald <drew@watershedclimate.com>
2024-01-15 13:53:20 -0500

    make it a pwa.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,44 @@
 import { fileURLToPath, URL } from "url";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+      },
+      manifest: {
+        name: "mood.",
+        short_name: "mood.",
+        description: "Simple and immersive one-click mood logging.",
+        theme_color: "#94A3B8",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     // https://stackoverflow.com/a/67676242
     alias: [

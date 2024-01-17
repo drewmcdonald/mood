36ce27853bf4c7f65358ea0f7276830c002e5caa
Drew McDonald <drew@watershedclimate.com>
2024-01-16 20:51:58 -0500

    whoops

---
+++ pwa-assets.config.ts
@@ -0,0 +1,29 @@
+import {
+  defineConfig,
+  minimal2023Preset as preset,
+  type Preset,
+} from "@vite-pwa/assets-generator/config";
+
+const minimalPresetNoPadding: Preset = {
+  transparent: {
+    sizes: [64, 192, 512],
+    favicons: [[48, "favicon.ico"]],
+    padding: 0,
+  },
+  maskable: {
+    sizes: [512],
+    padding: 0,
+  },
+  apple: {
+    sizes: [180],
+    padding: 0,
+  },
+};
+
+export default defineConfig({
+  headLinkOptions: {
+    preset: "2023",
+  },
+  preset: minimalPresetNoPadding,
+  images: ["public/logo.png"],
+});

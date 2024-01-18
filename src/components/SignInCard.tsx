62d44a72375a316f969ef1712b3ab2fa04f6e892
Drew McDonald <drew@watershedclimate.com>
2024-01-17 21:30:10 -0500

    enable github signin.

--- src/components/SignInCard.tsx
+++ src/components/SignInCard.tsx
@@ -13,15 +13,23 @@ import { Button } from "./ui/button";
 import { GitHubIcon } from "./vend/GitHubIcon";
 import { GoogleIcon } from "./vend/GoogleIcon";
 import { Separator } from "./ui/separator";
+import { useSbClient } from "@mood/hooks/useSbClient";
 
 export default function SignInCard({ className }: { className?: string }) {
+  const client = useSbClient();
   return (
     <Card className={className}>
       <CardHeader>
         <CardTitle className="text-2xl">Sign In</CardTitle>
       </CardHeader>
       <CardContent>
-        <Button variant="outline" className="mb-2 w-full" disabled>
+        <Button
+          variant="outline"
+          className="mb-2 w-full"
+          onClick={() =>
+            void client.auth.signInWithOAuth({ provider: "github" })
+          }
+        >
           <GoogleIcon className="mr-2 h-4 w-4" />
           Sign in with Google
         </Button>

da04d9a00b02b4a8408ec1e698d951a5f554bc63
Drew McDonald <drew@watershedclimate.com>
2024-01-17 21:12:06 -0500

    fix button layouts.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { useSessionContext } from "@supabase/auth-helpers-react";
 import { MoodGrid } from "./components/MoodGrid";
 import { Spinner } from "@mood/components/Spinner";
 import { LogOutButton } from "./components/LogOutButton";
+import { RecentsDialog } from "./components/RecentsDialog";
+import { FloatingButtonWithTooltip } from "./components/shared/FloatingButtonWithTooltip";
+import { History } from "lucide-react";
 
 function App() {
   const { session, isLoading } = useSessionContext();
@@ -13,6 +16,17 @@ function App() {
   return (
     <div className="p-4">
       <MoodGrid />
+      <RecentsDialog
+        trigger={
+          <FloatingButtonWithTooltip
+            tooltip="Recents"
+            bottom={28}
+            right={8}
+            Icon={History}
+            onClick={() => null}
+          />
+        }
+      />
       <LogOutButton />
     </div>
   );
--- src/components/shared/FloatingButtonWithTooltip.tsx
+++ src/components/shared/FloatingButtonWithTooltip.tsx
@@ -24,11 +24,15 @@ export function FloatingButtonWithTooltip({
 }) {
   return (
     <Tooltip>
-      <TooltipTrigger className={`fixed bottom-${bottom} right-${right}`}>
+      <TooltipTrigger
+        asChild
+        className={`fixed bottom-${bottom} right-${right}`}
+      >
         <Button
           aria-label={tooltip}
           className={cn(
-            "h-16 w-16 animate-fade-in-95 rounded-full bg-slate-200 opacity-95 shadow-md transition-all duration-300 ease-in-out hover:-translate-x-3 hover:scale-105 hover:bg-current hover:bg-opacity-95 hover:shadow-xl dark:bg-slate-700",
+            `fixed h-16 w-16 animate-fade-in-95 rounded-full bg-slate-200 opacity-95 shadow-md transition-all duration-300 ease-in-out hover:-translate-x-3 hover:scale-105 hover:bg-current hover:bg-opacity-95 hover:shadow-xl dark:bg-slate-700`,
+            `bottom-${bottom} right-${right}`,
             className,
           )}
           onClick={onClick}
@@ -38,7 +42,7 @@ export function FloatingButtonWithTooltip({
       </TooltipTrigger>
       <TooltipContent
         side="left"
-        className="text-md mr-4 animate-fade-in-95 text-primary opacity-95 shadow-md"
+        className="mb-2 animate-fade-in-95 text-primary opacity-95 shadow-md"
       >
         {tooltip}
       </TooltipContent>
